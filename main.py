8d4150586c15a1946788c9b7132350e51cb16555
n3wbi-dev <bartosz.korz@gmail.com>
2021-05-13 15:10:35 +0200

    Initial commit

--- main.py
+++ main.py
@@ -2,19 +2,25 @@ from random import randint
 import string
 import os
 
-
+#Copy the text to clipboard
 def add_text_to_clipboard(text):
-    command = 'echo ' + text.strip() + '| clip'
+    command = 'echo ' + text.strip() + ' | clip'
     os.system(command)
+    print("Copied to clipboard!")
 
-
+#We need a list with all characters so we can select a random character from it
 allCharacters = list(string.ascii_letters + string.digits + string.punctuation)
+
 password = ""
-characterNumber = int(input("Welcome to password generator. How many characters do you want in your password? "))
+
+#User determines length of the password
+characterNumber = int(input("Welcome to password generator. Enter how many characters do you want in your password: "))
+
+#Generating password
 for x in range(characterNumber):
     password += allCharacters[randint(0,len(allCharacters)-1)]
 print(f"Generated password: {password}")
-userInput = input("Do you want to copy this password to clipboard? Y/N: ")
-if userInput.lower() == "y":
-    add_text_to_clipboard(password)
-    print("Copied to clipboard!")
+copyPassToClipboard = input("Do you want to copy this password to clipboard? Y/N: ")
+if copyPassToClipboard.lower() == "y" : add_text_to_clipboard(password)
+    
+    
